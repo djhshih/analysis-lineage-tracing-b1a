2e1b7b3b4004a795e71b0140a70142686724a606
David J. H. Shih <djh.shih@gmail.com>
2021-08-13 00:18:31 -0500

    Finished scripts.

--- analyze.R
+++ analyze.R
@@ -1,9 +1,131 @@
 library(io)
 library(ggplot2)
+library(dplyr)
 
-hec <- qread("data/hec.rds");
-hsc <- qread("data/hsc.rds");
+hec.w <- qread("data/hec_wide.rds");
+hsc.w <- qread("data/hsc_wide.rds");
 
-ggplot(hec, aes(t_injection, bm_fl.hsc)) +
+
+ggplot(hec.w, aes(t_injection, bm_fl.hsc)) + 
+	theme_classic() +
 	geom_point()
+
+ggplot(hec.w, aes(t_analysis, bm_fl.hsc)) +
+	theme_classic() +
+	geom_point()
+
+ggplot(hec.w, aes(t_injection, t_analysis, size=bm_fl.hsc)) +
+	theme_classic() +
+	geom_point()
+
+
+# ---
+
+hec.wn <- qread("data/normalized/hec_wide_normalized.rds");
+hsc.wn <- qread("data/normalized/hsc_wide_normalized.rds");
+
+# @param d  table in wide format
+# compartment: location.cell (e.g. bm_fl.hsc)
+ratio_trace_plot <- function(d, cell) {
+	d$ratio <- d[[cell]];
+	g <- ggplot(d, aes(t_analysis, ratio)) +
+		theme_classic() +
+		geom_hline(yintercept=1, colour="grey60") +
+		stat_smooth(method="glm") +
+		geom_point() +
+		ggtitle(cell)
+
+	if (length(unique(d$group)) > 1) {
+		g <- g + facet_grid(group ~ ., scales="free_y");
+	}
+
+	g
+}
+
+ratio_trace_plot(hec.wn, "bm_fl.hsc")
+ratio_trace_plot(hec.wn, "bm_fl.st_hsc")
+ratio_trace_plot(hec.wn, "bm_fl.mpp")
+ratio_trace_plot(hec.wn, "bm_fl.pro_b")
+ratio_trace_plot(hec.wn, "bm_fl.pre_b")
+
+grep("b1a", names(hec.wn), value=TRUE)
+ratio_trace_plot(hec.wn, "peritoneal.b1a")
+ratio_trace_plot(hec.wn, "spleen.b1a")
+
+# examine E7.5
+hec.wn.e7.5 <- filter(hec.wn, group == "E7.5");
+ratio_trace_plot(hec.wn.e7.5, "bm_fl.hsc")
+ratio_trace_plot(hec.wn.e7.5, "bm_fl.mpp")
+ratio_trace_plot(hec.wn.e7.5, "bm_fl.pro_b")
+ratio_trace_plot(hec.wn.e7.5, "peritoneal.b1a")
+
+# re-analyze peritoneal.b1a after removing the lone outlier
+outlier <- filter(hec.wn, group == "E7.5", peritoneal.b1a > 5)$id;
+filter(hec.wn, id %in% outlier);
+ratio_trace_plot(filter(hec.wn.e7.5, ! id %in% outlier), "peritoneal.b1a")
+
+
+ratio_trace_plot(hsc.wn, "bm_fl.hsc")
+ratio_trace_plot(hsc.wn, "bm_fl.st_hsc")
+ratio_trace_plot(hsc.wn, "bm_fl.mpp")
+ratio_trace_plot(hsc.wn, "bm_fl.pro_b")
+ratio_trace_plot(hsc.wn, "bm_fl.pre_b")
+
+
+# ---
+
+hec.l <- qread("data/hec_long.rds");
+hsc.l <- qread("data/hsc_long.rds");
+
+# @param d  table in long format
+count_trace_plots <- function(d) {
+	ggplot(d, aes(t_analysis, count, colour=cell)) +
+		theme_classic() +
+		facet_grid(group ~ cell) +
+		ylim(0, 100) +
+		guides(colour="none") +
+		stat_smooth(method="glm") +
+		geom_point()
+}
+
+count_trace_plots(filter(hec.l, site == "bm_fl"));
+count_trace_plots(filter(hec.l, site == "spleen"));
+count_trace_plots(filter(hec.l, site == "thymus"));
+
+count_trace_plots(filter(hsc.l, site == "bm_fl"));
+count_trace_plots(filter(hsc.l, site == "spleen"));
+count_trace_plots(filter(hsc.l, site == "thymus"));
+
+# ---
+
+hec.ln <- qread("data/normalized/hec_long_normalized.rds");
+hsc.ln <- qread("data/normalized/hsc_long_normalized.rds");
+
+unique(hec.ln$site)
+
+# @param d  table in long format
+ratio_trace_plots <- function(d, ylim=NULL) {
+	ggplot(d, aes(t_analysis, ratio, colour=cell)) +
+		theme_classic() +
+		geom_hline(yintercept=1, colour="grey60") +
+		guides(colour="none") +
+		coord_cartesian(ylim = ylim) +
+		facet_grid(group ~ cell) +
+		stat_smooth(method="glm") +
+		geom_point()
+}
+
+ratio_trace_plots(filter(hec.ln, site == "bm_fl"), ylim=c(0, 4));
+ratio_trace_plots(filter(hec.ln, site == "neonatal_spleen"));
+ratio_trace_plots(filter(hec.ln, site == "spleen"));
+ratio_trace_plots(filter(hec.ln, site == "thymus"));
+ratio_trace_plots(filter(hec.ln, site == "peritoneal_cavity"), ylim=c(0, 8));
+
+ratio_trace_plots(filter(hsc.ln, site == "bm_fl"));
+ratio_trace_plots(filter(hsc.ln, site == "neonatal_spleen"));
+ratio_trace_plots(filter(hsc.ln, site == "spleen"));
+ratio_trace_plots(filter(hsc.ln, site == "thymus"));
+ratio_trace_plots(filter(hsc.ln, site == "peritoneal_cavity"));
+
+# ---
 
--- prepare.R
+++ prepare.R
@@ -2,6 +2,7 @@ library(io)
 library(magrittr)
 library(danitizer)
 library(dplyr)
+library(tidyr)
 
 hec <- qread("data/hec", na.strings="N/A");
 hsc <- qread("data/hsc", na.strings="N/A");
@@ -47,27 +48,123 @@ hec$mouse$t_injection <- hec$mouse$t_injection - dpc.birth;
 hec$mouse <- select(hec$mouse, id, t_injection, t_analysis, sex, dose_tam, group)
 hsc$mouse <- select(hsc$mouse, id, t_injection, t_analysis, sex, dose_tam, group)
 
+remove_id <- function(xs) {
+	with(xs,
+		list(
+			mouse = mouse,
+			bm_fl = select(bm_fl, -id),
+			neonatal_spleen = select(neonatal_spleen, -id),
+			spleen = select(spleen, -id),
+			thymus = select(thymus, -id),
+			peritoneal_cavity = select(peritoneal_cavity, -id)
+		)
+	)
+}
 
-# join tables
+devel_stage_factor <- function(groups) {
+	levels <- unique(groups);
+	stage <- ifelse(grepl("E", levels), 0, 1);
+	days <- as.numeric(gsub("(E|P)(\\d+).*", "\\2", levels));
+	days.end <- suppressWarnings(as.numeric(gsub(".*-(\\d+)", "\\1", levels)));
+	idx <- order(stage, days, days.end);
+	factor(groups, levels[idx])
+}
 
-hec.d <- with(hec, data.frame(
-	mouse,
-	bm_fl = select(bm_fl, -id),
-	neonatal_spleen = select(neonatal_spleen, -id),
-	spleen = select(spleen, -id),
-	thymus = select(thymus, -id),
-	peritoneal = select(peritoneal_cavity, -id)
-));
+fix_devel_stages <- function(xs) {
+	# remove problematic stages
+	idx <- xs$mouse$group != "E0";
+	ys <- lapply(xs, function(x) x[idx, ]);
 
-hsc.d <- with(hsc, data.frame(
-	mouse,
-	bm_fl = select(bm_fl, -id),
-	neonatal_spleen = select(neonatal_spleen, -id),
-	spleen = select(spleen, -id),
-	thymus = select(thymus, -id),
-	peritoneal = select(peritoneal_cavity, -id)
-));
+	ys$mouse$group <- devel_stage_factor(ys$mouse$group);
 
-qwrite(hec.d, "data/hec.rds");
-qwrite(hsc.d, "data/hsc.rds");
+	ys
+}
+
+hec <- fix_devel_stages(hec);
+hsc <- fix_devel_stages(hsc);
+
+qwrite(hec, "data/hec.rds");
+qwrite(hsc, "data/hsc.rds");
+
+# ---
+
+# combine tables column-wise
+combine_tables <- function(xs) {
+	with(remove_id(xs), data.frame(
+		mouse,
+		bm_fl = bm_fl,
+		neonatal_spleen = neonatal_spleen,
+		spleen = spleen,
+		thymus = thymus,
+		peritoneal = peritoneal_cavity
+	))
+}
+
+
+hec.w <- combine_tables(hec);
+hsc.w <- combine_tables(hsc);
+
+
+qwrite(hec.w, "data/hec_wide.rds");
+qwrite(hsc.w, "data/hsc_wide.rds");
+
+
+# stack tables
+# main: main table name
+stack_tables <- function(xs, main, values_to="count") {
+	others <- setdiff(names(xs), main);
+	names(others) <- others;
+	ys <- do.call(rbind, lapply(others,
+		function(other) {
+			pivot_longer(xs[[other]], !id, names_to = "cell", values_to=values_to, values_drop_na = TRUE) %>%
+				mutate(site = other)
+		}
+	));
+	
+	zs <- left_join(xs[[main]], ys, by="id");
+	zs[!is.na(zs[[values_to]]), ]
+}
+
+hec.l <- stack_tables(hec, "mouse");
+hsc.l <- stack_tables(hsc, "mouse");
+
+qwrite(hec.l, "data/hec_long.rds");
+qwrite(hsc.l, "data/hsc_long.rds");
+
+
+
+normalize_wrt_hsc <- function(xs, min.val=1) {
+	xs$bm_fl$hsc[which(xs$bm_fl$hsc < min.val)] <- NA;
+
+	c(
+		xs["mouse"],
+		lapply(xs[! names(xs) %in% "mouse"],
+			function(x) {
+				as.data.frame(lapply(x, function(z) {
+					if (is.numeric(z)) {
+						z / xs$bm_fl$hsc
+					} else {
+						z
+					}
+				}))
+			}
+		)
+	)
+}
+
+hec.n <- normalize_wrt_hsc(hec);
+hsc.n <- normalize_wrt_hsc(hsc);
+
+hec.wn <- combine_tables(hec.n);
+hsc.wn <- combine_tables(hsc.n);
+
+qwrite(hec.wn, "data/normalized/hec_wide_normalized.rds");
+qwrite(hsc.wn, "data/normalized/hsc_wide_normalized.rds");
+
+
+hec.ln <- stack_tables(hec.n, main="mouse", values_to="ratio");
+hsc.ln <- stack_tables(hsc.n, main="mouse", values_to="ratio");
+
+qwrite(hec.ln, "data/normalized/hec_long_normalized.rds");
+qwrite(hsc.ln, "data/normalized/hsc_long_normalized.rds");
 
