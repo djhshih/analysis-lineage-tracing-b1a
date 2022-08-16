d712d2cf119753f59eee587b8b2b17e228fac593
David J. H. Shih <djh.shih@gmail.com>
2022-08-16 13:56:17 +0800

    Improved plots.

--- sim/hsc-number-abc.R
+++ sim/hsc-number-abc.R
@@ -1,22 +1,25 @@
 library(io)
 
-set.seed(1337)
-
 # times are in days
 tsize <- 24;
 
 # number of samples
-#N <- 5e7;
-N <- 1e7;
+N <- 5e7;
+#N <- 1e7;
 
 # number of cell types
 J <- 5;
 
+cells <- c("HSC", "ST-HSC", "MMP", NA, "CLP");
 # HSC, ST-HSC, MMP, CMP, CLP
 target <- c(4291, 10562, 99220, NA, 44683);
 #target <- c(4291, NA, NA, NA, NA);
 target.sd <- c(791, 695, 25019, NA, 30116);
-delta <- qnorm(1 - 0.01/2);
+alpha <- 0.01;
+delta <- qnorm(1 - alpha/2);
+
+resume <- TRUE;
+accepted.fn <- "accepted.rds";
 
 thresholds <- delta * target.sd / target;
 
@@ -114,7 +117,13 @@ sim <- function(params) {
 	n
 }
 
-accepted <- list();
+if (resume) {
+	set.seed();
+	accepted <- qread(accepted.fn);
+} else {
+	accepted <- list();
+}
+
 for (i in 1:N) {
 
 	if (i %% 1000 == 0) {
@@ -132,17 +141,101 @@ for (i in 1:N) {
 	}
 }
 
-n0 <- unlist(lapply(accepted, function(x) x$n0));
-beta1 <- unlist(lapply(accepted, function(x) x$beta[1])) * 24;
-alpha.out1 <- unlist(lapply(accepted, function(x) x$alpha.out[1])) * 24;
-
 length(accepted)
 
-hist(n0)
-hist(beta1)
+n0 <- unlist(lapply(accepted, function(x) x$n0));
+beta1 <- unlist(lapply(accepted, function(x) x$beta[1])) * tsize;
+alpha.out1 <- unlist(lapply(accepted, function(x) x$alpha.out[1])) * tsize;
+
+betas <- matrix(unlist(lapply(accepted, function(x) x$beta)), ncol=length(accepted)) * tsize;
+
+rowMeans(betas)
+
+plot(betas[1, ], betas[2, ])
+
+ns <- matrix(unlist(lapply(accepted, function(x) x$n)), ncol=length(accepted));
+pred <- rowMeans(ns)
+pred.sd <- apply(ns, 1, sd);
+
+library(ggplot2)
+library(ggsci)
+
+d <- rbind(
+	data.frame(
+		x = factor(cells, levels=cells[!is.na(cells)]),
+		y = target,
+		ymin = target - target.sd,
+		ymax = target + target.sd,
+		group = "observed"
+	),
+	data.frame(
+		x = factor(cells, levels=cells[!is.na(cells)]),
+		y = pred,
+		ymin = pred - pred.sd,
+		ymax = pred + pred.sd,
+		group = "predicted"
+	)
+);
+
+d <- d[complete.cases(d), ];
+
+qdraw(
+	ggplot(d, aes(x=x, y=y, ymin=ymin, ymax=ymax, fill=group)) + theme_classic() +
+		geom_col(position=position_dodge()) + 
+		geom_errorbar(width=0.3, position=position_dodge(width=0.9)) +
+		scale_y_continuous() +
+		scale_fill_npg() +
+		theme(legend.title=element_blank()) +
+		xlab("") + ylab("cell number")
+	,
+	file = "hsc-number_abc-fit.pdf"
+)
+
+mean(n0)
+print(quantile(n0, c(0.025, 0.975)))
+
+qdraw(
+	ggplot(data.frame(x=n0), aes(x=x)) + theme_classic() +
+		geom_histogram(binwidth=3) +
+		#geom_vline(xintercept=quantile(n0, c(0.025, 0.975)), col="firebrick") +
+		xlab("initial HSC number")
+	,
+	file="hsc-number_n0_hist.pdf"
+)
+
+hist(n0, breaks=50)
+plot(density(n0, from=1, to=100, bw=4))
+hist(beta1, breaks=50)
 plot(n0, beta1)
-hist(n0[beta1 < 2])
+
+smoothScatter(n0, beta1)
+
+d <- MASS::kde2d(n0, beta1, n=60);
+qdraw(
+	{
+		contour(d,
+			col=hcl.colors(12, "viridis"),
+			las=1,
+			ylab="net prolif. rate of HSC",
+			xlab="initial HSC number",
+			ylim=c(0, 4)
+		)
+		#points(n0, beta1, pch=20, col="#00000020")
+	},
+	file = "hsc-number_contour_n0-beta1.pdf"
+);
+
+hist(n0[beta1 < 3], breaks=100)
+
+quantile(n0, c(0.05))
+mean(n0 <= 1)
+summary(n0)
+
+idx <- n0 == 1;
+beta1[idx]
+
 hist(alpha.out1)
+plot(n0, alpha.out1)
 dev.off()
 
 qwrite(accepted, "accepted.rds")
